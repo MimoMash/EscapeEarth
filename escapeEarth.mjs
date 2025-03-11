17c02d69ed2a2514c57d6478846ec808bf0a953f
MimoMash <mimomashyt@gmail.com>
2025-03-11 19:08:47 +0100

    added code to fetch info from RIS and display the first challenge

--- escapeEarth.mjs
+++ escapeEarth.mjs
@@ -6,7 +6,13 @@ const API = "https://api.le-systeme-solaire.net/rest/";
 
 async function start() {
   console.log("Starting mission");
-  
+
+  const startResponse = await fetch(`${RIS}/start?player=${EMAIL}`);
+
+  const mission = await startResponse.json();
+  console.log("Mission received:", mission.message);
+  console.log("Challenge:", mission.challenge);
+
 }
 
 start();
