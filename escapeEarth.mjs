7822c599f5e2d9c2595b1f77eabf1766fc3f649a
MimoMash <mimomashyt@gmail.com>
2025-03-11 19:42:10 +0100

    changed API path to find all the bodies

--- escapeEarth.mjs
+++ escapeEarth.mjs
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 const EMAIL = "mohammaa@uia.no"; 
 const RIS = "https://spacescavanger.onrender.com";
-const API = "https://api.le-systeme-solaire.net/rest/";
+const API = "https://api.le-systeme-solaire.net/rest/bodies/";
 
 async function start() {
   console.log("Starting mission");
