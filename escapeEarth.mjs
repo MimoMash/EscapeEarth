c02f99547b5ae48529bb6f6c50bfff6779e20aa4
MimoMash <mimomashyt@gmail.com>
2025-03-11 19:01:55 +0100

    imported fetch, made constants and started on main game function

---
+++ escapeEarth.mjs
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch';
+
+const EMAIL = "mohammaa@uia.no"; 
+const RIS = "https://spacescavanger.onrender.com";
+const API = "https://api.le-systeme-solaire.net/rest/";
+
+async function start() {
+  console.log("Starting mission");
+  
+}
+
+start();
